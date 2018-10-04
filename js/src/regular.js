031bfe6380bd6072b6dc5a65639a650ba1347bc3
euni <soleeyaa@gmail.com>
2018-10-04 22:35:08 +0900

    js

--- js/src/regular.js
+++ js/src/regular.js
@@ -3,11 +3,9 @@
 $(window).on('scroll',function(e){
 	e.preventDefault();
 	var thisTop=$(this).scrollTop();
-	if(thisTop<1000){
+	if(thisTop<500){
 		topBtn.stop().show();
-	}else{
-		topBtn.stop().hide();
-	};
+	}
 });
 
 topBtn.on('click',function(e){
