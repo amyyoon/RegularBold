181ead2bfb3d60dbcda293573dd2c1bc46229007
euni <soleeyaa@gmail.com>
2018-10-05 16:17:31 +0900

    update

--- js/src/regular.js
+++ js/src/regular.js
@@ -12,4 +12,23 @@ topBtn.on('click',function(e){
 	e.preventDefault();
 	$('html','body').animate({scrollTop:0},500);
 });
+
+var contentUl =$('.contentUl');
+var contentLi =contentUl.children('li');
+
+contentLi.on('mouseover',function(e){
+	e.preventDefault();
+	var i = $(this).index();
+	contentLi.eq(i).addClass('active');
+	contentLi.eq(i).children('dl').addClass('show');
+
+});
+
+contentLi.on('mouseleave',function(e){
+	e.preventDefault();
+	var i = $(this).index();
+	contentLi.eq(i).removeClass('active');
+	contentLi.eq(i).children('dl').removeClass('show');
+});
+
 })(jQuery);
